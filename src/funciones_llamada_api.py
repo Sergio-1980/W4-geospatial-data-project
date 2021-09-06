dde0ca29e19d214410c7f0aa1f015f168d307675
Sergio-1980 <sgpnovoa@gmail.com>
2021-09-06 09:15:49 +0200

    subir

--- src/funciones_llamada_api.py
+++ src/funciones_llamada_api.py
@@ -1,20 +1,37 @@
+# Librerias importadas token
+
+
 import requests
+import json
 
-class geocode:
-    
-    token = os.getenv("Client_Id")
-    token2 = os.getenv("Client_Secret")
 
-    def geocode_coord(direccion):
-        """
-        Esta función saca las coordenadas de la dirección que le pases
-        """
-        data = requests.get(f"https://geocode.xyz/{direccion}?json=1").json()
-        try:
-            return {
-                "type": "Point",
-                "coordinates": [data["latt"], data["longt"]]
-            }
+
+def geocode_coord(direccion):
+    """
+    Esta función saca las coordenadas de la dirección que le pases
+    """
+    data = requests.get(f"https://geocode.xyz/{direccion}?json=1").json()
+    try:
+        return {
+            "type": "Point",
+            "coordinates": [data["latt"], data["longt"]]
+        }
+    except:
+        return data
     
-        except:
-            return data
+def generar_json(diccionario, name, token, token2):
+    
+    url_query = 'https://api.foursquare.com/v2/venues/explore'
+    
+    parametros = {
+        "client_id": token,
+        "client_secret": token2,
+        "v": "20180323",
+        "ll": f"{diccionario['coordinates'][0]}, {diccionario['coordinates'][1]}",
+        "query": name,
+        "limit": 200
+    }
+    
+    resp = requests.get(url_query, params=parametros).json()
+    
+    return resp
